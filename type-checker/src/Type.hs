262b6905bcef726b624a6e6acf3bbc545dfa8c4f
patrickZWY <154947644+patrickZWY@users.noreply.github.com>
2026-05-03 00:16:41 -0400

    infer

--- type-checker/src/Type.hs
+++ type-checker/src/Type.hs
@@ -1,6 +1,7 @@
 module Type where
-import GHC.Natural (Natural)
+import GHC.Natural (Natural, naturalFromInteger)
 import Syntax (Expr(..))
+import GHC.TypeLits (Nat)
 
 data Ty
     = TVar Natural
@@ -189,6 +190,119 @@ unify ty1 ty2 = case (ty1, ty2) of
         UnifyFail $
             "Failed to unify types: "
             ++ show ty1 ++ " and " ++ show ty2
+
+data InferResult
+    = InferOk Subst Ty Natural
+    | InferFail String
+    deriving (Show, Eq)
+
+-- inferSubst :: InferResult -> Subst
+-- inferSubst (InferOk subst _ _) = subst
+-- inferSubst _ = error "Inference failed, no substitution available"
+
+-- inferType :: InferResult -> Ty
+-- inferType (InferOk _ ty _) = ty
+-- inferType _ = error "Inference failed, no type available"
+
+-- inferNext :: InferResult -> Natural
+-- inferNext (InferOk _ _ next) = next
+-- inferNext _ = error "Inference failed, no next type variable available"
+
+freshTyVar :: Natural -> (Ty, Natural)
+freshTyVar next = (TVar next, next + 1)
+
+envExtend :: String -> Ty -> TyEnv -> TyEnv
+envExtend name ty (TyEnv env) = TyEnv ((name, ty) : env)
+
+inferTrue :: TyEnv -> Expr -> Natural -> InferResult
+inferTrue _ ETrue next = InferOk (Subst []) TBool next
+inferTrue _ _ _ = InferFail "Expected ETrue expression"
+
+inferFalse :: TyEnv -> Expr -> Natural -> InferResult
+inferFalse _ EFalse next = InferOk (Subst []) TBool next
+inferFalse _ _ _ = InferFail "Expected EFalse expression"
+
+inferVar :: TyEnv -> Expr -> Natural -> InferResult
+inferVar env (Var name) next = case envLookup name env of
+    Just ty -> InferOk (Subst []) ty next
+    Nothing -> InferFail ("Unbound variable: " ++ name)
+inferVar _ _ _ = InferFail "Expected Var expression"
+
+inferLam :: TyEnv -> Expr -> Natural -> InferResult
+inferLam env (Lam var body) next =
+    let (paramTy, next1) = freshTyVar next
+        env' = envExtend var paramTy env
+    in case infer env' body next1 of
+        InferFail msg -> InferFail ("Failed to infer lambda body: " ++ msg)
+        InferOk subst bodyTy next2 ->
+            -- param needs to be updated with the substitution because it
+            -- may contain type variables that were unified during the inference of the body 
+            let funTy = mkTFun (applySubstTy subst paramTy) bodyTy
+            in InferOk subst funTy next2
+inferLam _ _ _ = InferFail "Expected Lam expression"  
+
+inferApp :: TyEnv -> Expr -> Natural -> InferResult
+inferApp env (App fun arg) next =
+    case infer env fun next of
+        InferFail msg -> InferFail ("Failed to infer function: " ++ msg)
+        InferOk substFun funTy next1 ->
+            case infer (applySubstEnv substFun env) arg next1 of
+                InferFail msg -> InferFail ("Failed to infer argument: " ++ msg)
+                InferOk substArg argTy next2 ->
+                    let tmp = freshTyVar next2
+                        (resultTy, next3) = tmp
+                        -- this is the type we construct for the function with unknown result type
+                        funTyExpected = mkTFun argTy resultTy
+                    -- unify it with the inferred body type of the function (updated by substitution)
+                    in case unify (applySubstTy substArg funTy) funTyExpected of
+                        UnifyFail msg -> InferFail ("Failed to unify function type: " ++ msg)
+                        UnifyOk substUnify ->
+                            -- substUnify is the latest substitution
+                            let subst = composeSubst substUnify (composeSubst substArg substFun)
+                            in InferOk subst resultTy next3
+inferApp _ _ _ = InferFail "Expected App expression"       
+
+
+inferIf :: TyEnv -> Expr -> Natural -> InferResult
+inferIf env (If cond thenBranch elseBranch) next =
+    case infer env cond next of
+        InferFail msg -> InferFail ("Failed to infer condition: " ++ msg)
+        InferOk substCond condTy next1 ->
+            case unify condTy TBool of
+                UnifyFail msg -> InferFail ("Condition must be of type Bool: " ++ msg
+                    ++ " Inferred type: " ++ show condTy)
+                UnifyOk substUnifyCond ->
+                    let subst1 = composeSubst substUnifyCond substCond 
+                    in case infer (applySubstEnv subst1 env) thenBranch next1 of
+                        InferFail msg -> InferFail ("Failed to infer then branch: " ++ msg)
+                        InferOk substThen thenTy next2 ->
+                            case infer (applySubstEnv substThen (applySubstEnv subst1 env)) elseBranch next2 of
+                                InferFail msg -> InferFail ("Failed to infer else branch: " ++ msg)
+                                InferOk substElse elseTy next3 ->
+                                    case unify (applySubstTy substElse thenTy) elseTy of
+                                        UnifyFail msg -> InferFail ("Then and Else branch must be of same type: " ++ msg
+                                            ++ " Then branch: " ++ show (applySubstTy substElse thenTy) ++ " Else branch: "
+                                            ++ show elseTy)
+                                        UnifyOk substUnifyElse ->
+                                            let s = foldr1 composeSubst [substUnifyElse, substElse, substThen, substUnifyCond, substCond]
+                                                in InferOk s (applySubstTy s elseTy) next
+inferIf _ _ _ = InferFail "Unexpected If expression"
+
+infer :: TyEnv -> Expr -> Natural -> InferResult
+infer env expr next = case expr of
+    ETrue -> inferTrue env expr next
+    EFalse -> inferFalse env expr next
+    Var _ -> inferVar env expr next
+    Lam _ _ -> inferLam env expr next
+    App _ _ -> inferApp env expr next
+    If {} -> inferIf env expr next
+
+
+
+
+
+
+
 
 
 
